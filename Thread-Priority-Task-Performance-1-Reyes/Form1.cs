28a9b4ba777f9e8c3c488a45536ec2523b46ee3b
dncdnl <danicadaniela.cesicar@gmail.com>
2025-09-29 19:27:13 +0800

    Creating threads named Thread A,B,C,D

--- Thread-Priority-Task-Performance-1-Reyes/Form1.cs
+++ Thread-Priority-Task-Performance-1-Reyes/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,19 @@ namespace Thread_Priority_Task_Performance_1_Reyes
         public frmTrackThread()
         {
             InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            // creating a thread object and passing the method to be executed by the thread
+            lblThread.Text = "-Thread Start-";
+            Console.WriteLine("-Thread Start-");
+
+            // creating 4 threads name
+            Thread threadA = new Thread(MyThreadClass.Thread1);
+            Thread threadB = new Thread(MyThreadClass.Thread1);
+            Thread threadC = new Thread(MyThreadClass.Thread2);
+            Thread threadD = new Thread(MyThreadClass.Thread2);
         }
     }
 }
