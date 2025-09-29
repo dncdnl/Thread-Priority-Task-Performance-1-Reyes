e94cb9ee9e3f99757ae1b04cf9a0bf894c1414b9
dncdnl <danicadaniela.cesicar@gmail.com>
2025-09-29 19:15:20 +0800

    Creating a loop in side the thread1 method to count the loop 2 times and added a .5 seconds suspenssion

--- Thread-Priority-Task-Performance-1-Reyes/MyThreadClass.cs
+++ Thread-Priority-Task-Performance-1-Reyes/MyThreadClass.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Thread_Priority_Task_Performance_1_Reyes
 {
@@ -10,7 +11,12 @@ namespace Thread_Priority_Task_Performance_1_Reyes
     {
         public static void Thread1()
         {
-
+            for(int x =0; x <= 2; x++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread:  " + thread.Name + " = " + x );
+                Thread.Sleep(500);
+            }
         }
         public static void  Thread2()
         {
