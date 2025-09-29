3c07d498696f620a4ca26e5a80f6b74493f7e267
dncdnl <danicadaniela.cesicar@gmail.com>
2025-09-29 19:07:50 +0800

    Craeting a class called My Thread Class and adding two static methods named thread1 and Thread2

---
+++ Thread-Priority-Task-Performance-1-Reyes/MyThreadClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread_Priority_Task_Performance_1_Reyes
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+
+        }
+        public static void  Thread2()
+        {
+
+        }
+    }
+}
